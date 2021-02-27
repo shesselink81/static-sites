018893193ee37cbe42f9277ba4d23cdaac74791d
Sander Hesselink <sander@hesselink.me>
2021-02-28 00:14:07 +0100

    update cadvisor dockerfile

--- cadvisor-armv7/docker-build.cmd
+++ cadvisor-armv7/docker-build.cmd
@@ -1,2 +1,2 @@
-docker build --platform linux/arm/v7 --tag quay.io/shesselink81/cadvisor:v0.37.5-armv7 -f .\cadvisor-armv7\Dockerfile .
+docker build --platform linux/arm/v7 --tag quay.io/shesselink81/cadvisor:v0.37.5-armv7 -f .\cadvisor-armv7\Dockerfile --no-cache .
 docker push quay.io/shesselink81/cadvisor:v0.37.5-armv7
