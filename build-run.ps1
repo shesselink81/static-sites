d4336a84e64f0e9fbe45f6d9836eb4e00e371439
Sander Hesselink <sander@hesselink.me>
2025-12-20 22:29:52 +0100

    add build script and changed port

---
+++ build-run.ps1
@@ -0,0 +1,4 @@
+docker compose --project-directory .\anna-nginx\ build
+docker compose --project-directory .\anna-nginx\ up -d
+docker compose --project-directory .\hesselinkme-nginx\ build
+docker compose --project-directory .\hesselinkme-nginx\ up -d
